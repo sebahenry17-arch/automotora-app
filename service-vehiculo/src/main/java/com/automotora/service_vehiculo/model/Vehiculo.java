cd0f84b55f65946fc70ca9a185a3b9c0b96023c8
sebahenry17-archgit config --globla user.email sebahenry17@gmail.com <sebahenry17@gmail.com>
2026-05-13 23:12:01 -0400

    Se completa la relacion de la clase vehiculo con tipoVehiculo y FichaVehiculo

--- service-vehiculo/src/main/java/com/automotora/service_vehiculo/model/Vehiculo.java
+++ service-vehiculo/src/main/java/com/automotora/service_vehiculo/model/Vehiculo.java
@@ -1,9 +1,14 @@
 package com.automotora.service_vehiculo.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,5 +29,10 @@ public class Vehiculo
     private String estado;
     private int stock;
 
-    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_tipo_vehiculo")
+    private TipoVehiculo tipoVehiculo;
+
+    @OneToOne(mappedBy = "vehiculo", cascade = CascadeType.ALL)
+    private FichaVehiculo ficha;    
 }
