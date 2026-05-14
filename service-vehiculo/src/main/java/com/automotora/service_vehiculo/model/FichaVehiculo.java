fb23386320fb72ac2f2ce75f7208fa5f266002b1
sebahenry17-archgit config --globla user.email sebahenry17@gmail.com <sebahenry17@gmail.com>
2026-05-13 23:09:05 -0400

    se crea clase ficha vehiculo

---
+++ service-vehiculo/src/main/java/com/automotora/service_vehiculo/model/FichaVehiculo.java
@@ -0,0 +1,33 @@
+package com.automotora.service_vehiculo.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FichaVehiculo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String numeroSerie;
+    private String color;
+    private String combustible;
+    private String transmision;
+    private LocalDate fechaMatricula;
+
+    @OneToOne
+    @JoinColumn(name = "vehiculo_id")
+    private Vehiculo vehiculo;
+}
