cfff8c056182461866ddb403e4f0fe12101582be
sebahenry17-archgit config --globla user.email sebahenry17@gmail.com <sebahenry17@gmail.com>
2026-05-17 16:05:04 -0400

    se sube el service ficha con el properties modificado

---
+++ service-ficha/src/main/java/com/automotora/service_ficha/ServiceFichaApplication.java
@@ -0,0 +1,13 @@
+package com.automotora.service_ficha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFichaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFichaApplication.class, args);
+	}
+
+}
---
+++ service-ficha/src/test/java/com/automotora/service_ficha/ServiceFichaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automotora.service_ficha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFichaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
