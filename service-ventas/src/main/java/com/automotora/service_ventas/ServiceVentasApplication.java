aea66bce547b1a2636b8d39dbc5a6555ea7edd14
sebahenry17-archgit config --globla user.email sebahenry17@gmail.com <sebahenry17@gmail.com>
2026-05-17 21:33:39 -0400

    Se crea clase modelo venta y el config con el webclient y se configura el pom

---
+++ service-ventas/src/main/java/com/automotora/service_ventas/ServiceVentasApplication.java
@@ -0,0 +1,13 @@
+package com.automotora.service_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceVentasApplication.class, args);
+	}
+
+}
---
+++ service-ventas/src/main/java/com/automotora/service_ventas/model/Venta.java
@@ -0,0 +1,37 @@
+package com.automotora.service_ventas.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fecha;
+    private Double monto;
+
+    
+    private Long clienteId;
+    private Long fichaId;
+
+    
+    @Transient
+    private Object datosCliente;
+
+    @Transient
+    private Object datosFichaVehiculo;
+}
