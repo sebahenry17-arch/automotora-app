2599be82652006a68c5fcf94df3c253dc81a5c1a
sebahenry17-archgit config --globla user.email sebahenry17@gmail.com <sebahenry17@gmail.com>
2026-06-21 17:21:05 -0400

    se le agrega el @Repository al empleadoRepository

--- service-empleado/src/main/java/com/automotora/service_empleado/repository/EmpleadoRepository.java
+++ service-empleado/src/main/java/com/automotora/service_empleado/repository/EmpleadoRepository.java
@@ -1,9 +1,11 @@
 package com.automotora.service_empleado.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.automotora.service_empleado.model.Empleado;
 
+@Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 
   
